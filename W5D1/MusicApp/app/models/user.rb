47e60c6731a479bee1241a6586ed6dd0c2a47ad0
jeffren716 <jeffyiren@gmail.com>
2017-10-30 22:52:46 -0700

    finish polymorphic associations

---
+++ W5D1/MusicApp/app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ApplicationRecord
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.correct_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def correct_password?(password)
+    pass_hash = BCrypt::Password.new(self.password_digest)
+    pass_hash.is_password?(password)
+  end
+
+end
